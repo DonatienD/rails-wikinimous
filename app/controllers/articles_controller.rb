0e4223eb498e474c0df96456dbdd1f53516d0ca3
Donatien D <ddlu.web@icloud.com>
2020-11-17 17:03:18 +0100

    Delete (#destroy) implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -26,6 +26,12 @@ class ArticlesController < ApplicationController
     @article.update(article_params)
 
     redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
